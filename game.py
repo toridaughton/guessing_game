cbfde0809d6693b1ed9598863d669160fd69b9ce
Tori Daughton <toriddaughton@gmail.com>
2022-05-17 22:10:00 -0600

    initial commit

---
+++ game.py
@@ -0,0 +1,22 @@
+import random
+
+def numberGuess():
+    count = 0
+    new_player_name = input("Yoo-hoo, what's your name?\n(type in your name) ")
+    greeting = print(new_player_name + ", I'm thinking of a number between 1 and 100.")
+    computer_number = random.randint(1, 100)
+    guess = int(input("Try to guess my number. "))
+
+    while(guess != computer_number):
+        count += 1 
+        if guess > computer_number:
+            print("Sorry! Your guess is too high, try again.")
+            guess = int(input("Your new guess? "))
+        elif guess < computer_number:
+            print("Sorry! Your guess is too low, try again.")
+            guess = int(input("Your new guess? "))
+
+    if(guess == computer_number):
+        print("Congratulations! You guessed my number! It took you", count, "tries!")
+
+numberGuess()
